154d45a26ccaf9f1134731a1c06e53228549852b
zhirnovaAlisA <zirnovaalisa277@gmail.com>
2024-11-27 16:22:07 +0300

    2 задание

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,14 +17,14 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода символа вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+
+      document.addEventListener('keydown',(event) => {
+        if ((event.key.toLowerCase() == this.currentSymbol.textContent) || (event.key.toUpperCase() == this.currentSymbol.textContent)) {
+          this.success();
+        }else{
+          this.fail();
+        }
+      });
   }
 
   success() {
