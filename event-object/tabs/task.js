ccfea7389005a85b2b7bdb87d7b1545587ae8238
zhirnovaAlisA <zirnovaalisa277@gmail.com>
2024-11-29 18:28:51 +0300

    задание 3 2.0

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,16 +1,16 @@
 const headers = document.querySelectorAll(".tab");
 const paragraphs = document.querySelectorAll(".tab__content")
-const masHeaders = Array.from(headers);
-const masParagraphs = Array.from(paragraphs);
 
-for( let i = 0; i < headers.length; i ++){
+function tabsChanger(index) {
+    for (let j = 0; j < headers.length; j++) {
+        paragraphs[j].classList.remove("tab__content_active");
+        headers[j].classList.remove("tab_active");
+    }
+    paragraphs[index].classList.add("tab__content_active");
+    headers[index].classList.add("tab_active");
+}
+
+for (let i = 0; i < headers.length; i++) {
     let header = headers[i];
-    header.addEventListener("click", () => {
-        for ( let j = 0; j < headers.length; j ++){
-            paragraphs[j].classList.remove("tab__content_active");
-            headers[j].classList.remove("tab_active");
-        }
-        paragraphs[i].classList.add("tab__content_active");
-        headers[i].classList.add("tab_active");
-    })
+    header.addEventListener("click", () => tabsChanger(i));
 }
