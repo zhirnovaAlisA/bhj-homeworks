96f56a1eac4c3da71e96811ab95e680c5c78d0e5
zhirnovaAlisA <zirnovaalisa277@gmail.com>
2024-12-12 12:00:14 +0300

    tooltip 3.0

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,34 +1,32 @@
 const tipElements = document.querySelectorAll(".has-tooltip");
 
+const tooltipElement = document.createElement("div");
+tooltipElement.className = "tooltip";
+document.body.appendChild(tooltipElement);
+
 for (let tip of tipElements) {
     tip.addEventListener("click", (event) => {
         event.preventDefault();
 
-        let currentTipText = document.querySelector(".tooltip");
-        let thisTipText = tip.title;
+        const thisTipText = tip.getAttribute("title");
 
-        if (currentTipText?.textContent === thisTipText){
-            console.log("повторное нажатие");
-            currentTipText.classList.toggle("tooltip");
-        }else{
+        if (tooltipElement.classList.contains("tooltip_active") && tooltipElement.textContent === thisTipText) {
+            tooltipElement.classList.remove("tooltip_active");
+            return;
+        }
 
-        const tooltipElement = document.createElement("div");
-        tooltipElement.className = "tooltip";
-        tooltipElement.textContent = tip.getAttribute("title");
+        tooltipElement.textContent = thisTipText;
 
         const rect = tip.getBoundingClientRect();
         tooltipElement.style.left = `${rect.left}px`;
         tooltipElement.style.top = `${rect.bottom}px`;
 
-        document.body.appendChild(tooltipElement);
-
         tooltipElement.classList.add("tooltip_active");
-        }
     });
 }
 
 document.addEventListener("click", (event) => {
     if (!event.target.classList.contains("has-tooltip")) {
-        document.querySelectorAll('.tooltip').forEach(el => el.remove());
+        tooltipElement.classList.remove("tooltip_active");
     }
 });
